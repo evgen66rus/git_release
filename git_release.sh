d91105b08aaf5eb6869b86919181e9f17bf12370
evgen66rus <evgen.alyoshin@gmail.com>
2022-03-16 18:09:11 +0500

    fixed script

--- git_release.sh
+++ git_release.sh
@@ -4,22 +4,22 @@ version=$1
 text=$2
 branch=$(git rev-parse --abbrev-ref HEAD)
 repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
-token=$(git config --global github.token)
+token="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"
 
 generate_post_data()
 {
   cat <<EOF
-{
-  "tag_name": "$version",
-  "target_commitish": "$branch",
-  "name": "$version",
-  "body": "$text",
-  "draft": false,
-  "prerelease": false
-}
+    {
+      "tag_name": "$version",
+      "target_commitish": "$branch",
+      "name": "$version",
+      "body": "$text",
+      "draft": false,
+      "prerelease": false
+    }
 EOF
 }
 
 echo "Create release $version for repo: $repo_full_name branch: $branch"
-curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token
-"
+curl -v  -H "Authorization: token $token" --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases"
+
